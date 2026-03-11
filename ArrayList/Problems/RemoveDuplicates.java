2883e91c39050b182d8fcd13f896c71927e8cc26
KeshavSharma-dotcom <keshavsharma787705@gmail.com>
2026-03-11 19:24:30 +0530

    commit

--- ArrayList/Problems/RemoveDuplicates.java
+++ ArrayList/Problems/RemoveDuplicates.java
@@ -1,15 +1,6 @@
-void main(){
-    ArrayList<Integer> list = new ArrayList<>();
-    Scanner sc = new Scanner(System.in);
-    for(int i=0;i<5;i++){
-        list.add(sc.nextInt());
-    }
-    System.out.println();
-    System.out.println("Original: "+list);
-
+void RemoveDuplicates(ArrayList<Integer> list){
     ArrayList<Integer> newList = new ArrayList<>();
     HashSet<Integer> set = new HashSet<>();
-
     int i = 0;
     while(list.size()>i){
         int curr = list.get(i);
@@ -21,5 +12,16 @@ void main(){
     }
     System.out.println();
     System.out.println("Unique Elements: "+newList);
+}
 
+
+void main(){
+    ArrayList<Integer> list = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    for(int i=0;i<5;i++){
+        list.add(sc.nextInt());
+    }
+    System.out.println();
+    System.out.println("Original: "+list);
+    RemoveDuplicates(list);
 }
