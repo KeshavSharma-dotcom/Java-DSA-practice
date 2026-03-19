038858da7d373995642fa22685f9606e00f31bb2
KeshavSharma-dotcom <keshavsharma787705@gmail.com>
2026-03-19 22:47:35 +0530

    solved remove duplicate nodes and cycle detection in LL

--- LinkedList/BasicLinkedList.java
+++ LinkedList/BasicLinkedList.java
@@ -12,19 +12,17 @@ static class LinkedList{
         Node n1 = new Node(value);
         if(head == null){
             head = n1;
+            tail = n1;
             return;
         }
-        Node temp = head;
-
-        while(temp.next != null){
-            temp = temp.next;
-        }
-        temp.next = n1;
+        tail.next = n1;
+        tail = n1;
     }
     public void insertStarting(int val){
         Node n1 = new Node(val);
         if(head == null){
             head = n1;
+            tail = n1;
             return;
         }
         n1.next = head;
@@ -37,13 +35,11 @@ static class LinkedList{
         Node end = new Node(val);
         if(head == null){
             head = end;
+            tail = end;
             return;
         }
-        Node temp = head;
-        while(temp.next != null){
-            temp = temp.next;
-        }
-        temp.next = end;
+        tail.next = end;
+        tail = end;
     }
     public void deleteFromStart(){
         if(head == null){
@@ -55,6 +51,12 @@ static class LinkedList{
     public void deleteFromEnd(){
         if(head == null){
             System.out.println("List is empty");
+            tail = null;
+            return;
+        }
+        if(head.next == null){
+            head = null;
+            tail = null;
             return;
         }
         Node temp = head;
@@ -62,6 +64,7 @@ static class LinkedList{
             temp = temp.next;
         }
         temp.next = null;
+        tail = temp;
     }
     public void printList(){
 
@@ -74,15 +77,50 @@ static class LinkedList{
             System.out.print(temp.data+" ");
             temp = temp.next;
         }
+        System.out.println();
+    }
+    public LinkedList removeDuplicate(LinkedList l){
+        if(head == null){
+            return l;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        LinkedList newList = new LinkedList();
+        Node temp = head;
+        while(temp != null){
+            if(set.add(temp.data)){
+                newList.insertElement(temp.data);
+            }
+            temp = temp.next;
+        }
+        return newList;
+    }
+    public boolean hasCycle(){
+        if(head == null) return false;
+        Node slow = head;
+        Node fast  = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
     }
 }
-
 
 void main() {
     LinkedList l1 = new LinkedList();
     l1.insertElement(10);
     l1.insertElement(20);
     l1.insertElement(30);
-    l1.insertElement(40);
+    l1.insertElement(10);
+    l1.insertElement(50);
+    l1.insertElement(30);
     l1.printList();
+    LinkedList uniqueNodes = l1.removeDuplicate(l1);
+    uniqueNodes.printList();
+
+    System.out.println("List cycle detection result: "+ l1.hasCycle());
 }
