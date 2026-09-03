2bd216c990815f3633e45dfda92a25c5d565280d
KeshavSharma-dotcom <keshavsharma787705@gmail.com>
2026-09-03 21:14:04 +0530

    O(n)

---
+++ Array/Problems/Leet3876.java
@@ -0,0 +1,18 @@
+public boolean uniformArray(int[] nums1){
+    int min = Integer.MAX_VALUE;
+    boolean hasOdd = false;
+
+    for (int x : nums1) {
+        if (x < min) {
+            min = x;
+        }
+        if ((x & 1) != 0) {
+            hasOdd = true;
+        }
+    }
+
+    return !hasOdd || (min % 2 != 0);
+}
+void main(){
+
+}
